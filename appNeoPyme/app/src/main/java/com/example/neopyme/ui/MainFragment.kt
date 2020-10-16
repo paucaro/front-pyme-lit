6806e682e096294f44dcf27d2a44f606e3b9c940
paucaro <carolinaasto@gmail.com>
2020-10-16 18:30:29 -0500

    Adding app

---
+++ appNeoPyme/app/src/main/java/com/example/neopyme/ui/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.example.neopyme.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.neopyme.R
+import com.example.neopyme.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var navController: NavController = Navigation.findNavController(view)
+
+        preevaluar_button.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_resultadosFragment)
+        }
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
